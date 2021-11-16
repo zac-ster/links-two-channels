ea1bde1332fdace574f12ed36049fdf24814b9fa
ZacBotDev <60801799+zac-ster@users.noreply.github.com>
2021-11-16 12:18:11 +0700

    Update main.py

--- main.py
+++ main.py
@@ -35,5 +35,6 @@ async def on_message(msg):
 
           await webhook1.send(content=f"{msg.content}", username=msg.author.name, avatar_url=msg.author.avatar_url)
 
-keep_alive.keep_alive() #Remove this if you're hosting the bot in heroku
+if connfig.Website == True:
+  keep_alive.keep_alive()
 bot.run(config.token)
